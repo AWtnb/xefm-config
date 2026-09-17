26d5136f00366f8385ff0192d9102a0467e8420f
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-09-17 16:57:14 +0900

    update

--- .xefm/preference/__init__.py
+++ .xefm/preference/__init__.py
@@ -1,7 +1,16 @@
+from pathlib import Path
+
 from . import favorite, keybind, theme
+from ._utils import smart_check_path
 
 THEMES = {"Phosphor": theme.phosphor_theme}
 
 KEY_BINDINGS = keybind.bindings
 
-FAVORITE_DIRECTORIES = favorite.get_okini_bookmarks() or []
+OKINI_BOOKMARKS = favorite.get_okini_bookmarks() or []
+
+FAVORITE_DIRECTORIES = [
+    fav
+    for fav in OKINI_BOOKMARKS
+    if smart_check_path(fav["path"]) and Path(fav["path"]).is_dir()
+]
